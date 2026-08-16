53b30bed1413d7a5c0d21e05e50223764154b847
Rayssa Farago <rayssa.farago@escola.pr.gov.br>
2026-08-16 18:08:09 +0000

    Criação do categoriaRoutes

---
+++ backend /src/routes/categoriaRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/categoriaController');
+const autenticar = require('../middlewares/auth');
+ 
+router.use(autenticar); 
+ 
+router.get('/', ctrl.listar);
+router.get('/:id', ctrl.buscar);
+router.post('/', ctrl.criar);
+router.put('/:id', ctrl.atualizar);
+router.delete('/:id', ctrl.excluir);
+ 
+module.exports = router;
